a41db8740dc1c9c0e7d19cf7f529720641817851
ChristianLGartman <115482491+ChristianLGartman@users.noreply.github.com>
2022-12-20 01:44:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,31 +2,30 @@
 #include<cmath>
 using namespace std;
 
-double f (double y) {
-    double f;
+double f (double y, double fx) {
     y=(y/180)*M_PI;
-    f=pow(y, 2)*cos(y)+1;
-    return f;
+    fx=y*y*cos(y)+1;
+    return fx;
 }
 
 
 int main() {
-    double a=0,b=0,x=0,err;
-    while(f(a)*f(b)>=0) {
+    double a=0,b=0,x=0,err, fx=0;
+    while(f(a,fx)*f(b,fx)>=0) {
         cout <<"inserire estremi"<<endl;
         cin >> a >> b;
 
-        if (f(a)*f(b) < 0){
+        if (f(a, fx)*f(b,fx) < 0){
             break;
         }
     }
 
-    while(f(x)!=0){
+    while(f(x,fx)!=0){
         x=(a+b)/2;
 
-        if (f(a) * f(b) < 0) {
+        if (f(a,fx) * f(b,fx) < 0) {
             b = x;
-        } 
+        }
         else {
             a = x;
         }
