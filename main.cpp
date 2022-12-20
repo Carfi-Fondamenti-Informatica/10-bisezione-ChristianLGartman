eaf069b49679a0666948d6bb7e40c26a921ba8ad
ChristianLGartman <115482491+ChristianLGartman@users.noreply.github.com>
2022-12-20 01:59:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,43 +1,39 @@
 #include <iostream>
-#include<cmath>
+#include <cmath>
 using namespace std;
-
-double f (double y, double fx) {
-    y=(y/180)*M_PI;
-    fx=y*y*cos(y)+1;
+double funzione(double x , double &fx)
+{
+    fx = x * x * cos(x) + 1;
     return fx;
 }
 
-
 int main() {
-    double a=0,b=0,x=0,err, fx=0;
-   while(f(a,fx)*f(b,fx)>=0) {
+double a=0,b=0,fx=0,x=0,err=2;
+while(funzione(a,fx)*funzione(b,fx)>=0) {
     cout <<"inserire estremi"<<endl;
     cin >> a >> b;
 
-    if (f(a,fx)*f(b,fx) < 0){
+    if (funzione(a,fx)*funzione(b,fx) < 0){
         break;
     }
 }
 
-    while(f(x,fx)!=0){
-        x=(a+b)/2;
-        if (f(x,fx) * f(b,fx) < 0) {
-            b = x;
-        }
-        else {
+while (funzione(x,fx)!=0) {
+    x=(a+b)/2;
+        if (funzione(x, fx) * funzione(b, fx) < 0) {
             a = x;
         }
-        err = abs((b - a) / 2);
-
+        else{
+        b = x;
+        }
+    err = abs((a - b)) / 2;
         if (err < 1e-6){
             break;
         }
-    }
-    x=x*10000;
-    x=(int)x;
-    x=x/10000;
-    cout<< x;
-
-    return 0;
+}
+x=x*10000;
+x=(int)x;
+x=x/10000;
+cout<< x;
+return 0;
 }
