565b3bf76d8c013eeac1b283b9a5f464c0d690c2
ChristianLGartman <115482491+ChristianLGartman@users.noreply.github.com>
2022-12-20 01:39:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,46 +1,45 @@
 #include <iostream>
-#include <cmath>
+#include<cmath>
 using namespace std;
-double f(double x , double &fx)
-{
-    fx = x * x * cos(x) + 1;
-    return fx;
+
+double f (double y) {
+    double f;
+    y=(y/180)*M_PI;
+    f=pow(y, 2)*cos(y)+1;
+    return f;
 }
 
+
 int main() {
-    double a=0,b=0,fx=0,x=0,err;
-    while(f(a,fx)*f(b,fx)>=0) {
+    double a=0,b=0,x=0,err;
+    while(f(a)*f(b)>=0) {
         cout <<"inserire estremi"<<endl;
         cin >> a >> b;
 
-        if (f(a,fx)*f(b,fx) < 0){
+        if (f(a)*f(b) < 0){
             break;
         }
     }
 
-    while (f(x,fx)!=0) {
+    while(f(x)!=0){
         x=(a+b)/2;
-        if (f(x, fx) * f(b, fx) < 0) {
+
+        if (f(a) * f(b) < 0) {
+            b = x;
+        } 
+        else {
             a = x;
         }
-        else{
-            b = x;
-        }
-        err = abs((a - b)) / 2;
-        if (err < 1e-6){
-            break;}
-    }
+        err = abs((b - a) / 2);
 
+        if (err < 1e-6){
+            break;
+        }
+    }
     x=x*10000;
     x=(int)x;
     x=x/10000;
     cout<< x;
 
-
-
-
     return 0;
-
-
-
 }
